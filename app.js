d16d4bbf0a0e050153f984e133c2f540c9033132
missalissa <alissa.beddow@gmail.com>
2017-10-30 17:38:58 -0700

    created hourlySales function; defined hourlySales object properties; called hourlySales

--- app.js
+++ app.js
@@ -1,52 +1,77 @@
 'use strict';
 
-function custPerHour(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
-
-function salesPerHour() {
-    const sales = custPerHour(this.min, this.max) * this.avg;
-    return sales;
-}
-
 const pdx = {
     min: 23,
     max: 65,
     avg: 6.3,
-    custRate: custPerHour,
-    salesRate: salesPerHour
-}
-
-pdx.custRate(pdx.min, pdx.max);
-console.log(pdx.custRate(pdx.min, pdx.max));
-
-pdx.salesRate(pdx.avg);
-console.log(pdx.salesRate(pdx.avg));
+    hourlySales: hourlySales
+    // calculates total
+    // render: function(){
+    //     const li = document.createElement('li');
+    //     li.textContent = this.hour + ' ' + this.salesByHour;
+    //     return li;
+    }
 
 const piosquare = {
     min: 3,
     max: 24,
-    avg: 1.2
+    avg: 1.2,
+    hourlySales: hourlySales  
 }
 
 const powells = {
     min: 11,
     max: 38,
-    avg: 3.7
+    avg: 3.7,
+    hourlySales: hourlySales    
 }
 
 const stjohns = {
     min: 20,
     max: 38,
-    avg: 2.3
+    avg: 2.3,
+    hourlySales: hourlySales    
 }
 
 const waterfront = {
     min: 2,
     max: 16,
-    avg: 4.6
+    avg: 4.6,
+    hourlySales: hourlySales    
 }
+
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-const stores = [pdx, piosquare, powells, stjohns, waterfront];
+
+function hourlySales() {
+    const salesByHour = [];
+    for(let i = 0; i < hours.length; i++) {
+        const hour = hours[i];
+        const numCustomers = getRandomIntInclusive(this.min, this.max);
+        const numCookies = Math.floor(numCustomers * this.avg);
+        const oneHour = {
+            hour: hour,
+            cookiesSold: numCookies
+        };
+        salesByHour.push(oneHour);
+    }    
+    this.salesByHour = salesByHour;
+}
+
+pdx.hourlySales();
+piosquare.hourlySales();
+powells.hourlySales();
+stjohns.hourlySales();
+waterfront.hourlySales();
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+
+
+// console.log(pdx);
+
+
+// const stores = [pdx, piosquare, powells, stjohns, waterfront];
