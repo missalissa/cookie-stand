240514048e32ef83a1e9171af974b2f40b559a24
missalissa <alissa.beddow@gmail.com>
2017-10-31 18:04:58 -0700

    created render function to display all store data

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const pdx = {
     max: 65,
     avg: 6.3,
     hourlySales: hourlySales,
-    // calculates total
-    // render: render
+    salesByHour: [],
+    render: render
 }
 
 const piosquare = {
@@ -15,8 +15,9 @@ const piosquare = {
     min: 3,
     max: 24,
     avg: 1.2,
-    hourlySales: hourlySales,
-    // render: render
+    hourlySales: hourlySales,    
+    salesByHour: [],
+    render: render
 }
 
 const powells = {
@@ -24,8 +25,9 @@ const powells = {
     min: 11,
     max: 38,
     avg: 3.7,
-    hourlySales: hourlySales,
-    // render: render
+    hourlySales: hourlySales,    
+    salesByHour: [],
+    render: render
 }
 
 const stjohns = {
@@ -33,8 +35,9 @@ const stjohns = {
     min: 20,
     max: 38,
     avg: 2.3,
-    hourlySales: hourlySales,
-    // render: render  
+    hourlySales: hourlySales,    
+    salesByHour: [],
+    render: render  
 }
 
 const waterfront = {
@@ -42,11 +45,14 @@ const waterfront = {
     min: 2,
     max: 16,
     avg: 4.6,
-    hourlySales: hourlySales,
-    // render: render
+    hourlySales: hourlySales,    
+    salesByHour: [],
+    render: render
 }
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const stores = [pdx, piosquare, powells, stjohns, waterfront];
+const elStores = document.getElementById('stores');
 
 function hourlySales() {
     const salesByHour = [];
@@ -75,118 +81,118 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
+// created object literals for each store instance
 // object literal, pdx
-const pdxdiv = document.getElementById('pdx-store');
-const pdxh2 = document.createElement('h2');
-pdxh2.textContent = pdx.name;
-pdxdiv.appendChild(pdxh2);
-console.log(pdxdiv);
-console.log(pdxh2);
+// const pdxdiv = document.getElementById('pdx-store');
+// const pdxh2 = document.createElement('h2');
+// pdxh2.textContent = pdx.name;
+// pdxdiv.appendChild(pdxh2);
+// console.log(pdxdiv);
+// console.log(pdxh2);
 
-const pdxul = document.getElementById('pdx-list');
-for (let i = 0; i < hours.length; i++) {
-    const pdxli = document.createElement('li');
-    pdxli.textContent = hours[i] + ': ' + pdx.salesByHour[i].cookiesSold + ' cookies';
-    pdxh2.appendChild(pdxul);
-    pdxul.appendChild(pdxli); 
-}
+// const pdxul = document.getElementById('pdx-list');
+// for (let i = 0; i < hours.length; i++) {
+//     const pdxli = document.createElement('li');
+//     pdxli.textContent = hours[i] + ': ' + pdx.salesByHour[i].cookiesSold + ' cookies';
+//     pdxh2.appendChild(pdxul);
+//     pdxul.appendChild(pdxli); 
+// }
 
-// object literal, piosquare
-const piosquarediv = document.getElementById('piosquare-store');
-const piosquareh2 = document.createElement('h2');
-piosquareh2.textContent = piosquare.name;
-piosquarediv.appendChild(piosquareh2);
-console.log(piosquarediv);
-console.log(piosquareh2);
+// // object literal, piosquare
+// const piosquarediv = document.getElementById('piosquare-store');
+// const piosquareh2 = document.createElement('h2');
+// piosquareh2.textContent = piosquare.name;
+// piosquarediv.appendChild(piosquareh2);
+// console.log(piosquarediv);
+// console.log(piosquareh2);
 
-const piosquareul = document.getElementById('piosquare-list');
-for (let i = 0; i < hours.length; i++) {
-    const piosquareli = document.createElement('li');
-    piosquareli.textContent = hours[i] + ': ' + piosquare.salesByHour[i].cookiesSold + ' cookies';
-    piosquareh2.appendChild(piosquareul);
-    piosquareul.appendChild(piosquareli); 
-}
+// const piosquareul = document.getElementById('piosquare-list');
+// for (let i = 0; i < hours.length; i++) {
+//     const piosquareli = document.createElement('li');
+//     piosquareli.textContent = hours[i] + ': ' + piosquare.salesByHour[i].cookiesSold + ' cookies';
+//     piosquareh2.appendChild(piosquareul);
+//     piosquareul.appendChild(piosquareli); 
+// }
 
-// object literal, powells
-const powellsdiv = document.getElementById('powells-store');
-const powellsh2 = document.createElement('h2');
-powellsh2.textContent = powells.name;
-powellsdiv.appendChild(powellsh2);
-console.log(powellsdiv);
-console.log(powellsh2);
+// // object literal, powells
+// const powellsdiv = document.getElementById('powells-store');
+// const powellsh2 = document.createElement('h2');
+// powellsh2.textContent = powells.name;
+// powellsdiv.appendChild(powellsh2);
+// console.log(powellsdiv);
+// console.log(powellsh2);
 
-const powellsul = document.getElementById('powells-list');
-for (let i = 0; i < hours.length; i++) {
-    const powellsli = document.createElement('li');
-    powellsli.textContent = hours[i] + ': ' + powells.salesByHour[i].cookiesSold + ' cookies';
-    powellsh2.appendChild(powellsul);
-    powellsul.appendChild(powellsli); 
-}
+// const powellsul = document.getElementById('powells-list');
+// for (let i = 0; i < hours.length; i++) {
+//     const powellsli = document.createElement('li');
+//     powellsli.textContent = hours[i] + ': ' + powells.salesByHour[i].cookiesSold + ' cookies';
+//     powellsh2.appendChild(powellsul);
+//     powellsul.appendChild(powellsli); 
+// }
 
-// object literal, stjohns
-const stjohnsdiv = document.getElementById('stjohns-store');
-const stjohnsh2 = document.createElement('h2');
-stjohnsh2.textContent = stjohns.name;
-stjohnsdiv.appendChild(stjohnsh2);
-console.log(stjohnsdiv);
-console.log(stjohnsh2);
+// // object literal, stjohns
+// const stjohnsdiv = document.getElementById('stjohns-store');
+// const stjohnsh2 = document.createElement('h2');
+// stjohnsh2.textContent = stjohns.name;
+// stjohnsdiv.appendChild(stjohnsh2);
+// console.log(stjohnsdiv);
+// console.log(stjohnsh2);
 
-const stjohnsul = document.getElementById('stjohns-list');
-for (let i = 0; i < hours.length; i++) {
-    const stjohnsli = document.createElement('li');
-    stjohnsli.textContent = hours[i] + ': ' + stjohns.salesByHour[i].cookiesSold + ' cookies';
-    stjohnsh2.appendChild(stjohnsul);
-    stjohnsul.appendChild(stjohnsli); 
-}
+// const stjohnsul = document.getElementById('stjohns-list');
+// for (let i = 0; i < hours.length; i++) {
+//     const stjohnsli = document.createElement('li');
+//     stjohnsli.textContent = hours[i] + ': ' + stjohns.salesByHour[i].cookiesSold + ' cookies';
+//     stjohnsh2.appendChild(stjohnsul);
+//     stjohnsul.appendChild(stjohnsli); 
+// }
 
-// object literal, waterfront
-const waterfrontdiv = document.getElementById('waterfront-store');
-const waterfronth2 = document.createElement('h2');
-waterfronth2.textContent = waterfront.name;
-waterfrontdiv.appendChild(waterfronth2);
-console.log(waterfrontdiv);
-console.log(waterfronth2);
+// // object literal, waterfront
+// const waterfrontdiv = document.getElementById('waterfront-store');
+// const waterfronth2 = document.createElement('h2');
+// waterfronth2.textContent = waterfront.name;
+// waterfrontdiv.appendChild(waterfronth2);
+// console.log(waterfrontdiv);
+// console.log(waterfronth2);
 
-const waterfrontul = document.getElementById('waterfront-list');
-for (let i = 0; i < hours.length; i++) {
-    const waterfrontli = document.createElement('li');
-    waterfrontli.textContent = hours[i] + ': ' + waterfront.salesByHour[i].cookiesSold + ' cookies';
-    waterfronth2.appendChild(waterfrontul);
-    waterfrontul.appendChild(waterfrontli); 
-}
+// const waterfrontul = document.getElementById('waterfront-list');
+// for (let i = 0; i < hours.length; i++) {
+//     const waterfrontli = document.createElement('li');
+//     waterfrontli.textContent = hours[i] + ': ' + waterfront.salesByHour[i].cookiesSold + ' cookies';
+//     waterfronth2.appendChild(waterfrontul);
+//     waterfrontul.appendChild(waterfrontli); 
+// }
 
-
-
-    // for(let i = 0; i < 14; i++) {
-
-    //     }
-    // const li = document.createElement('li');
-
-// render function, revisit for constructor objects
+//render function, constructor objects
 // render();
 
-// function render () {
-//     const ul = document.getElementById('store-list');
-//     for (let i = 0; i < hours.length; i++) { 
-//         const li = document.createElement('li');
-//         ul.textContent = this.name;
-//         console.log(hours[i]);
-//         console.log(this.salesByHour[i]);
-//         li.textContent = hours[i] + ': ' + this.salesByHour[i].cookiesSold + ' cookies';
-//         li.classList.add('hourlySales');
-//         ul.appendChild(li);
-//     }
-// }
-// pdx.render ();
-// piosquare.render ();
-// powells.render ();
 
-    // function(){
-        // const ul = document.getElementById('store-list');
-        // const li = document.createElement('li');
-        //  li.textContent = this.hour + ' ' + this.salesByHour;
-        // //  ul.appendChild(li);
-        //  return li;
+function render (elParent) {
+    const elH2 = document.createElement('h2');
+    elH2.textContent = this.name;
+    elParent.appendChild(elH2);
+    const elUl = document.createElement('ul');
+    elParent.appendChild(elUl);
+
+    for (let i = 0; i < hours.length; i++) { 
+        const elLi = document.createElement('li');
+        console.log(this.salesByHour[i]);
+        elLi.textContent = hours[i] + ': ' + this.salesByHour[i].cookiesSold + ' cookies';
+        elUl.appendChild(elLi);
+    }
+};
+
+pdx.render(elStores);
+piosquare.render(elStores);
+powells.render(elStores);
+stjohns.render(elStores);
+waterfront.render(elStores);
+
+//     function(){
+//         const ul = document.getElementById('store-list');
+//         const li = document.createElement('li');
+//          li.textContent = this.hour + ' ' + this.salesByHour;
+//         //  ul.appendChild(li);
+//          return li;
 
 // render();
 // function render () {
@@ -197,4 +203,3 @@ for (let i = 0; i < hours.length; i++) {
 // }
 
 
-const stores = [pdx, piosquare, powells, stjohns, waterfront];
